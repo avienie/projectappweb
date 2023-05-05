9f6f0c996e6aa2abbf7780375fe251155056e4c2
avienie <lavinamavega18@gmail.com>
2023-05-05 10:46:01 +0700

    Project DPK

---
+++ script.js
@@ -0,0 +1,19 @@
+// Header toggle
+
+let MenuBtn = document.getElementById('MenuBtn')
+
+MenuBtn.addEventListener('click', function(e) {
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
+})
+
+
+// Typing Effect
+
+let typed = new Typed('.auto-input',{
+    strings: ['UI/UX Designer', 'Traveller', 'Content Creator', 'Student'],
+    typedSpeed: 100,
+    backSpeed:100,
+    backDelay: 2000,
+    loop: true,
+})
